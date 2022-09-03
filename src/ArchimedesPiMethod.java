4035819128a590ce60f3e807aa87bd9f7138a330
jfreedman340 <97777103+jfreedman340@users.noreply.github.com>
2022-09-03 00:01:10 -0700

    I wrote a program that estimates Pi using the Archimedes Method.

--- src/ArchimedesPiMethod.java
+++ src/ArchimedesPiMethod.java
@@ -2,6 +2,18 @@ import java.util.Scanner;
 
 public class ArchimedesPiMethod {
     public static void main(String[] args) {
-
+        //Number of sides.
+        int n = 8;
+        //Calculate the angle.
+        double B = 360.0 / n;
+        double A = 1.0 / 2.0 * B;
+        //Calculate length of the triangle.
+        double s = 2.0 * Math.toRadians(A);
+        //Get the polygon perimeter.
+        double p = n * s;
+        //Estimate PI.
+        double PI = p / 2.0;
+        //Print PI
+        System.out.println(PI);
     }
 }
